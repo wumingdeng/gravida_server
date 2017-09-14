666e7a5219b3c14a5cefec2ced901826ade8a000
Fizzo <809050631@qq.com>
2017-09-14 20:13:04 +0800

    happy birthday

---
+++ routes/imgUtils.js
@@ -0,0 +1,68 @@
+var multer = require('multer');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
+var yxdDB = require('../models_yxd');
+var mem = require('../memory')
+var util = require('../util/uitl.js')
+
+// var colors = []
+// var times = 0 //上传的图片遍历的次数的标识
+var storage2 = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var destDir = './static/storage/'+req.body.pid+'/';
+     // 判断文件夹是否存在
+    fs.stat(destDir, (err)=> {
+        if (err) {
+            // 创建文件夹
+            mkdirp(destDir, (err)=>{
+                if (err) {
+                    cb(err);
+                } else {
+                    cb(null, destDir);
+                }
+            });
+        } else {
+            cb(null, destDir);
+        }
+    });
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload2 = multer({ storage: storage2})
+
+
+function doInit(app){
+    // 带图片提交货号配置
+    app.post('/saveGoodCps',upload2.array('cps'),function(req,res,next){
+        var _id = req.body.id
+        var _pid = req.body.pid
+        var _color = req.body.color.toString()
+        var _size = req.body.size.toString()
+        var _name = req.body.name || ''
+        var filter = { pid: _pid, name: _name, color:_color,size:_size }
+        if (_id) {
+            filter.id = _id
+        }
+        var files = req.files
+        var fileStr = ''
+        for(var key in files){
+            var file = files[key]
+            //file.path.indexOf('static')+'static'.length+1 固定长度7
+            var name = file.path.substr(7)
+            fileStr += name+','
+        }
+        filter.pictures = fileStr.substr(0,fileStr.length-1)
+        console.log(filter.pictures)
+        util.checkRedisSessionId(req.sessionID, res, function (object) {
+            yxdDB.gravida_storage_configs.upsert(filter).then((data)=>{
+                mem.f.ReloadMemory('gravida_storage_configs',()=>{
+                    res.json({ok: mem.m.gravida_storage_configs});
+                })
+            })
+        })
+    });
+}
+
+module.exports = doInit
